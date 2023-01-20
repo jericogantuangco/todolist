b9a6a1441ec6d32d23878b193da844f2a8cc91ec
Jerico <jericogantuangco200@gmail.com>
2023-01-20 16:32:31 +0800

    added uuid

--- src/NewTodoForm.js
+++ src/NewTodoForm.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {v4 as uuidv4} from 'uuid';
 
 class NewTodoForm extends Component {
     constructor(props) {
@@ -17,7 +18,7 @@ class NewTodoForm extends Component {
     handleSubmit(evt) {
         evt.preventDefault();
         console.log('submitted')
-        this.props.createTodo(this.state);
+        this.props.createTodo({...this.state, id: uuidv4()});
         this.setState({task: ""});
     }
     render() {
