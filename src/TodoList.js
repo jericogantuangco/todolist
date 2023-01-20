f2551af2bb99529a246c549ebd154358fb2b6333
Jerico <jericogantuangco200@gmail.com>
2023-01-20 16:10:49 +0800

    added todolist component

---
+++ src/TodoList.js
@@ -0,0 +1,41 @@
+import React, {Component} from "react";
+import Todo from './Todo';
+import NewTodoForm from './NewTodoForm';
+
+class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: [
+                {
+                    task: 'Walk the Fish'
+                },
+                {
+                    task: "Groom Chickens"
+                }
+            ]
+        }
+        this.create = this.create.bind(this);
+    }
+
+    create(newTodo) {
+        this.setState({
+            todos: [...this.state.todos, newTodo]
+        });
+    }
+
+    render() {
+        const todos = this.state.todos.map( todo => <Todo task={todo.task} />)
+        return (
+            <div>
+                <h1>Todo List</h1>
+                <NewTodoForm createTodo={this.create} />
+                <ul>
+                    {todos}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default TodoList;
